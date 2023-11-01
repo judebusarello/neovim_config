85bdf194f0e698147343e448f8a25321ebf26d41
Jude Busarello <jude.busarello@vistarmedia.com>
2023-11-01 09:06:18 -0400

    Cut down on a bunch of LazyVim additions that were slowing down the works

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -14,50 +14,59 @@ _G.Diffviewopen = false
 --https://github.com/kevinhwang91/nvim-bqf
 --https://github.com/andersevenrud/nvim_context_vt
 return {
-  { "catppuccin/nvim",                     name = "catppuccin" },
-  { "junegunn/fzf",                        build = "./install --bin" },
-  { "HiPhish/nvim-ts-rainbow2",            enabled = true },  -- colorize parens and brackets
-  { "Bekaboo/deadcolumn.nvim",             enabled = true },  -- show colorcolumn as you approach
-  { "lukas-reineke/indent-blankline.nvim", enabled = true },  -- show colorcolumn as you approach
-  { "echasnovski/mini.comment",            enabled = true },  --comment out visual selection
-  { "L3MON4D3/LuaSnip",                    enabled = false }, -- Dunno what I'm doing with this
-  { "folke/trouble.nvim",                  enabled = false },
-  { "folke/todo-comments.nvim",            enabled = false },
-  { "echasnovski/mini.ai",                 enabled = false }, --don't know how to use
-  { "nvim-pack/nvim-spectre.nvim",         enabled = false },
-  { "folke/zen-mode.nvim",                 enabled = false },
-  { "folke/flash.nvim",                    enabled = false },
-  { "kdheepak/lazygit.nvim",               enabled = false },
-  { "goolord/alpha-nvim",                  enabled = false }, -- Welcome screen
-  { "echasnovski/mini.pairs",              enabled = false }, -- This autotypes paired bracus
-  { "echasnovski/mini.pairs",              enabled = false }, -- This autotypes paired bracus
-  { "nvim-neo-tree/neo-tree.nvim",         enabled = false }, -- I prefer the telescope extension for this stuff
-  { "folke/neodev.nvim",                   enabled = false },
-  { "nvim-lualine/lualine.nvim",           enabled = false }, -- don't like statuslines
-  { "echasnovski/mini.indentscope",        enabled = false }, -- the animated scope lines were distracting
-  { "SmiteshP/nvim-navic",                 enabled = false }, -- This shows data in the statusbar that I don't have
-  { "folke/trouble.nvim",                  enabled = false }, -- this is a list of all the diagnostic errors etc. Don't use it
-  { "RRethy/vim-illuminate",               enabled = false }, -- underlines all the same words as under the cursor. I find this distracting.
-  { "hrsh7th/cmp-buffer",                  enabled = false }, -- I don't want autocomplete to come from random words in the buffer
-  { "hrsh7th/cmp-path",                    enabled = false }, -- I don't use filesystem paths frequently. More likely to mess me up than help me out.
-  { "hrsh7th/cmp-vsnip",                   enabled = true },  -- I don't use filesystem paths frequently. More likely to mess me up than help me out.
-  { "hrsh7th/vim-vsnip",                   enabled = true },  -- I don't use filesystem paths frequently. More likely to mess me up than help me out.
-  { "saadparwaiz1/cmp_luasnip",            enabled = false }, -- I don't use luasnip. No need to have it for autocomplete
-  { "ggandor/leap.nvim",                   enabled = false }, -- I don't use easymotions
-  { "rmagatti/auto-session",               enabled = false }, -- I don't use easymotions
-  { "windwp/nvim-autopairs",               enabled = false }, -- This seems like I can make this work how I want
-  { "nvim-lua/plenary.nvim",               enabled = true },
-  { "nvim-telescope/telescope.nvim",       enabled = true },
-  {
-    "mrjones2014/op.nvim",
-    build = 'make install',
-    opts = {},
-    cmd = { "OpSidebar", "OpSignin", "OpNote" }
-  },
+  { "catppuccin/nvim",                             name = "catppuccin" },
+  { "junegunn/fzf",                                build = "./install --bin" },
+  { "HiPhish/nvim-ts-rainbow2",                    enabled = false }, -- colorize parens and brackets
+  { "Bekaboo/deadcolumn.nvim",                     enabled = true },  -- show colorcolumn as you approach
+  { "lukas-reineke/indent-blankline.nvim",         enabled = true },  -- show colorcolumn as you approach
+  { "echasnovski/mini.comment",                    enabled = true },  --comment out visual selection
+  { "L3MON4D3/LuaSnip",                            enabled = false }, -- Dunno what I'm doing with this
+  { "folke/trouble.nvim",                          enabled = false },
+  { "folke/todo-comments.nvim",                    enabled = false },
+  { "zbirenbaum/copilot-cmp",                      enabled = false },
+  { "zbirenbaum/copilot.lua",                      enabled = false },
+  { "MunifTanjim/nui.nvim",                        enabled = false },
+  { "folke/noice.nvim",                            enabled = false },
+  { "LazyVim/LazyVim",                             enabled = false },
+  { "rcarriga/nvim-notify",                        enabled = false },
+  { "echasnovski/mini.ai",                         enabled = false }, --don't know how to use
+  { "nvimdev/dashboard-nvim",                      enabled = false }, --don't know how to use
+  { "nvim-treesitter/nvim-treesitter-context",     enabled = false }, --don't know how to use
+  { "nvim-pack/nvim-spectre.nvim",                 enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false }, --don't know how to use
+  { "folke/zen-mode.nvim",                         enabled = false },
+  { "windwp/nvim-ts-autotag",                      enabled = false },
+  { "folke/flash.nvim",                            enabled = false },
+  { "kdheepak/lazygit.nvim",                       enabled = false },
+  { "goolord/alpha-nvim",                          enabled = false }, -- Welcome screen
+  { "echasnovski/mini.pairs",                      enabled = false }, -- This autotypes paired bracus
+  { "echasnovski/mini.pairs",                      enabled = false }, -- This autotypes paired bracus
+  { "nvim-neo-tree/neo-tree.nvim",                 enabled = false }, -- I prefer the telescope extension for this stuff
+  { "folke/neodev.nvim",                           enabled = false },
+  { "mfussenegger/nvim-lint",                      enabled = false },
+  { "stevearc/conform.nvim",                       enabled = false },
+  { "nvim-lualine/lualine.nvim",                   enabled = false }, -- don't like statuslines
+  { "echasnovski/mini.indentscope",                enabled = false }, -- the animated scope lines were distracting
+  { "SmiteshP/nvim-navic",                         enabled = false }, -- This shows data in the statusbar that I don't have
+  { "folke/trouble.nvim",                          enabled = false }, -- this is a list of all the diagnostic errors etc. Don't use it
+  { "RRethy/vim-illuminate",                       enabled = false }, -- underlines all the same words as under the cursor. I find this distracting.
+  { "hrsh7th/cmp-buffer",                          enabled = false }, -- I don't want autocomplete to come from random words in the buffer
+  { "hrsh7th/cmp-path",                            enabled = false }, -- I don't use filesystem paths frequently. More likely to mess me up than help me out.
+  { "hrsh7th/vim-vsnip",                           enabled = false }, -- I don't use filesystem paths frequently. More likely to mess me up than help me out.
+  { "saadparwaiz1/cmp_luasnip",                    enabled = false }, -- I don't use luasnip. No need to have it for autocomplete
+  { "ggandor/leap.nvim",                           enabled = false }, -- I don't use easymotions
+  { "rmagatti/auto-session",                       enabled = false }, -- I don't use easymotions
+  { "windwp/nvim-autopairs",                       enabled = false }, -- This seems like I can make this work how I want
+  { "nvim-lua/plenary.nvim",                       enabled = false },
+  { "nvim-telescope/telescope.nvim",               enabled = false },
   -- {
   --   "mrjones2014/op.nvim",
   --   enabled = true,
   --   build = "make install",
+  --   opts = {
+  --     signin_on_start = true,
+  --     biometric_unlock = false,
+  --   },
   --   cmd = {
   --     "OpAnalyzeBuffer",
   --     "OpCreate",
@@ -71,47 +80,43 @@ return {
   --     "OpView",
   --     "OpWhoami",
   --   },
-  --   opts = {
-  --     signin_on_start = true,
-  --   },
   -- },
   -- {
   --   "pwntester/octo.nvim",
   --   enabled = true,
   --   lazy = false,
-  --   config = function()
-  --     require("octo").setup({
-  --       picker = "fzf-lua",
-  --       gh_env = function()
-  --         local github_token =
-  --             require("op.api").item.get({ "GitHub", "--fields", "token" })[1]
-  --         if not github_token or not vim.startswith(github_token, "ghp_") then
-  --           error("Failed to get GitHub token.")
-  --         end
-  --         return { GITHUB_TOKEN = github_token }
-  --       end,
-  --     })
-  --   end,
+  --   opts = {
+  --     picker = "fzf-lua",
+  --     gh_env = function()
+  --       local github_token =
+  --           require("op.api").item.get({ "GitHub", "--fields", "token" })[1]
+  --       if not github_token or not vim.startswith(github_token, "ghp_") then
+  --         error("Failed to get GitHub token.")
+  --       end
+  --       return { GITHUB_TOKEN = github_token }
+  --     end,
+  --   }
   -- },
-  { "jose-elias-alvarez/null-ls.nvim",   enabled = false },
-  {
-    "nvimdev/guard.nvim",
-    enabled = true,
-    config = function()
-      local ft = require("guard.filetype")
-      ft("python"):fmt({
-        cmd = "/home/jude/vistar/ci/fmt-python",
-        args = { "fix" },
-        fname = false,
-        stdin = false,
-      })
-      require("guard").setup({})
-    end,
-    opts = {
-      fmt_on_save = true,
-      lsp_as_default_formatter = false,
-    },
-  },
+  -- { "jose-elias-alvarez/null-ls.nvim",     enabled = false },
+  -- { "elentok/format-on-save.nvim" },
+  -- {
+  --   "nvimdev/guard.nvim",
+  --   enabled = true,
+  --   config = function()
+  --     local ft = require("guard.filetype")
+  --     ft("python"):fmt({
+  --       cmd = "/home/jude/vistar/ci/fmt-python",
+  --       args = { "fix" },
+  --       fname = false,
+  --       stdin = false,
+  --     })
+  --     require("guard").setup({})
+  --   end,
+  --   opts = {
+  --     fmt_on_save = false,
+  --     lsp_as_default_formatter = false,
+  --   },
+  -- },
   {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -119,7 +124,7 @@ return {
       require("fzf-lua").setup({
         "max-perf",
         winopts = {
-          border = "FloatBorder",
+          -- border = "FloatBorder",
           width = 0.8,
           height = 0.7,
           preview = {
@@ -181,39 +186,39 @@ return {
   },
   {
     "nvim-treesitter",
-    enabled = true,
-    opts = {
-      rainbow = {
-        enable = true,
-      },
-    },
+    enabled = false,
+    -- opts = {
+    --   rainbow = {
+    --     enable = true,
+    --   },
+    -- },
   },
-  {
-    "folke/noice.nvim",
-    enabled = true,
-    opts = {
-      messages = {
-        enabled = true,            -- enables the Noice messages UI
-        view = "mini",             -- default view for messages
-        view_error = "mini",       -- view for errors
-        view_warn = "mini",        -- view for warnings
-        view_history = "messages", -- view for :messages
-        view_search = false,       -- view for search count messages. Set to `false` to disable
-      },
-      views = {
-        cmdline_popup = {
-          border = {
-            style = "none",
-            padding = { 1, 2 },
-          },
-          filter_options = {},
-          win_options = {
-            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   enabled = true,
+  --   opts = {
+  --     messages = {
+  --       enabled = true,            -- enables the Noice messages UI
+  --       view = "mini",             -- default view for messages
+  --       view_error = "mini",       -- view for errors
+  --       view_warn = "mini",        -- view for warnings
+  --       view_history = "messages", -- view for :messages
+  --       view_search = false,       -- view for search count messages. Set to `false` to disable
+  --     },
+  --     views = {
+  --       cmdline_popup = {
+  --         border = {
+  --           style = "none",
+  --           padding = { 1, 2 },
+  --         },
+  --         filter_options = {},
+  --         win_options = {
+  --           winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   {
     "akinsho/toggleterm.nvim",
     enabled = true,
@@ -309,31 +314,26 @@ return {
             == nil
       end
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end,
-        },
         window = {
           documentation = cmp.config.window.bordered(),
         },
         sorting = {
           priority_weight = 2,
-          comparators = {
-            require("copilot_cmp.comparators").prioritize,
-
-            -- Below is the default comparitor list and order for nvim-cmp
-            cmp.config.compare.offset,
-            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.locality,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
+          --   comparators = {
+          --     require("copilot_cmp.comparators").prioritize,
+          --
+          --     -- Below is the default comparitor list and order for nvim-cmp
+          --     cmp.config.compare.offset,
+          --     -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+          --     cmp.config.compare.exact,
+          --     cmp.config.compare.score,
+          --     cmp.config.compare.recently_used,
+          --     cmp.config.compare.locality,
+          --     cmp.config.compare.kind,
+          --     cmp.config.compare.sort_text,
+          --     cmp.config.compare.length,
+          --     cmp.config.compare.order,
+          --   },
         },
         mapping = cmp.mapping.preset.insert({
           [";"] = cmp.mapping.confirm({ select = true }),
@@ -354,43 +354,10 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "copilot" },
         }, {
           { name = "spell" },
         }),
       })
-    end,
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        python = true,
-        javascript = true,
-        typescript = true,
-        help = true,
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    opts = {},
-    config = function(_, opts)
-      local copilot_cmp = require("copilot_cmp")
-      copilot_cmp.setup(opts)
-      -- attach cmp source whenever copilot attaches
-      -- fixes lazy-loading issues with the copilot cmp source
-      require("lazyvim.util").on_attach(function(client)
-        if client.name == "copilot" then
-          copilot_cmp._on_insert_enter({})
-        end
-      end)
     end,
   },
   {
@@ -414,6 +381,12 @@ return {
       {
         "<leader>r",
         vim.lsp.buf.rename,
+        desc = "rename variable under cursor",
+        mode = "n",
+      },
+      {
+        "<leader>R",
+        vim.lsp.buf.references,
         desc = "rename variable under cursor",
         mode = "n",
       },
@@ -448,7 +421,7 @@ return {
       },
     },
   },
-  { "stevearc/dressing.nvim", enabled = true }, --this makes the rename dialog appear right over the thing you're renaming instead of the command line
+  { "stevearc/dressing.nvim",            enabled = true }, --this makes the rename dialog appear right over the thing you're renaming instead of the command line
   {
     "LazyVim/LazyVim",
     enabled = true,
@@ -514,7 +487,7 @@ return {
       --   mode = "n",
       -- },
       {
-        "<leader>b",
+        "<leader>bb",
         function()
           require("gitsigns").blame_line({ full = true })
         end,
